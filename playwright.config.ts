f0a084e3ebf7daf8b7f6626b94f5df11eaf1e236
Petr Fifka <petr.fifka@tredgate.com>
2023-09-20 15:35:55 +0200

    change timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   expect: { timeout: 3000 },
-  timeout: 5000,
+  timeout: 600000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
